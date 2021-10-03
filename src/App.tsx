fc755d4b661e1d2f1b1c2240743d2d3be6ee48b4
Steven Nguyen <stnguyen90@gmail.com>
2021-10-03 17:45:43 +0000

    Create login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,15 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import * as React from "react";
+import { ChakraProvider, CSSReset, theme } from "@chakra-ui/react";
+import { Appwrite } from "appwrite";
+import { Login } from "./pages/Login";
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+export const App = (): JSX.Element => {
+  const appwrite = new Appwrite();
+
+  return (
+    <ChakraProvider theme={theme}>
+      <CSSReset />
+      <Login appwrite={appwrite} />
+    </ChakraProvider>
+  );
+};
---
+++ src/pages/Login.tsx
@@ -0,0 +1,145 @@
+import * as React from "react";
+import {
+  Text,
+  Center,
+  Container,
+  Heading,
+  VStack,
+  InputGroup,
+  InputLeftElement,
+  Input,
+  Button,
+  FormControl,
+  InputRightElement,
+  IconButton,
+} from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import {
+  AtSignIcon,
+  InfoOutlineIcon,
+  LinkIcon,
+  LockIcon,
+  ViewIcon,
+  ViewOffIcon,
+} from "@chakra-ui/icons";
+import { Appwrite } from "appwrite";
+
+export const Login = (props: { appwrite: Appwrite }): JSX.Element => {
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClick = () => setShowPassword(!showPassword);
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  const onSubmit = async (values: {
+    endpoint: string;
+    project: string;
+    email: string;
+    password: string;
+  }) => {
+    const { endpoint, project, email, password } = values;
+
+    const { appwrite } = props;
+
+    appwrite.setEndpoint(endpoint);
+    appwrite.setProject(project);
+
+    const result = appwrite.account.createSession(email, password);
+    console.log(result);
+  };
+
+  return (
+    <Center minH="100vh" p={2}>
+      <Container
+        minH="md"
+        minW="xs"
+        backgroundColor="green"
+        borderWidth="1px"
+        borderRadius="md"
+      >
+        <Center minH="md">
+          <VStack w="full">
+            <Heading>Sign In</Heading>
+            <Text>Login using email and password</Text>
+            <form style={{ width: "100%" }} onSubmit={handleSubmit(onSubmit)}>
+              <VStack w="full">
+                <FormControl isInvalid={errors.endpoint}>
+                  <InputGroup>
+                    <InputLeftElement
+                      pointerEvents="none"
+                      children={<LinkIcon color="gray.300" />}
+                    />
+                    <Input
+                      id="endpoint"
+                      {...register("endpoint")}
+                      placeholder="Endpoint"
+                    />
+                  </InputGroup>
+                </FormControl>
+                <FormControl isInvalid={errors.project}>
+                  <InputGroup>
+                    <InputLeftElement
+                      pointerEvents="none"
+                      children={<InfoOutlineIcon color="gray.300" />}
+                    />
+                    <Input
+                      id="project"
+                      {...register("project")}
+                      placeholder="Project ID"
+                    />
+                  </InputGroup>
+                </FormControl>
+                <FormControl isInvalid={errors.email}>
+                  <InputGroup>
+                    <InputLeftElement
+                      pointerEvents="none"
+                      children={<AtSignIcon color="gray.300" />}
+                    />
+                    <Input
+                      id="email"
+                      {...register("email")}
+                      placeholder="Email"
+                    />
+                  </InputGroup>
+                </FormControl>
+                <FormControl isInvalid={errors.password}>
+                  <InputGroup size="md">
+                    <InputLeftElement
+                      pointerEvents="none"
+                      children={<LockIcon color="gray.300" />}
+                    />
+                    <Input
+                      pr="4.5rem"
+                      type={showPassword ? "text" : "password"}
+                      placeholder="Password"
+                      {...register("password")}
+                    />
+                    <InputRightElement>
+                      <IconButton
+                        aria-label={showPassword ? "Hide" : "Show"}
+                        h="1.75rem"
+                        w="1.25rem"
+                        size="xm"
+                        variant="link"
+                        color="gray.300"
+                        as={showPassword ? ViewOffIcon : ViewIcon}
+                        onClick={handleClick}
+                      />
+                    </InputRightElement>
+                  </InputGroup>
+                </FormControl>
+                <Button isLoading={isSubmitting} type="submit">
+                  Sign In
+                </Button>
+              </VStack>
+            </form>
+          </VStack>
+        </Center>
+      </Container>
+    </Center>
+  );
+};
